d29b6e6bf06f8254e4e86d5ad8c736af08a32edc
weikang.di <weikang.di@hongsong.club>
2022-05-10 01:04:37 +0800

    first commit

---
+++ netty-rpc-protocol/src/main/java/com/kang/netty/rpc/protocol/NettyRpcProtocolApplication.java
@@ -0,0 +1,13 @@
+package com.kang.netty.rpc.protocol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyRpcProtocolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyRpcProtocolApplication.class, args);
+    }
+
+}
---
+++ netty-rpc-provider/src/main/java/com/kang/netty/rpc/provider/NettyRpcProviderApplication.java
@@ -0,0 +1,13 @@
+package com.kang.netty.rpc.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyRpcProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyRpcProviderApplication.class, args);
+    }
+
+}
